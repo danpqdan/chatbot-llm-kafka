82388e1558980e12ec582eebe0ffbcc5074be6ff
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-03-01 22:31:32 -0300

    [ADD] primeiro commit para intergração de sistema de envio de email por fila

---
+++ src/main/java/com/email_queue/email_queue/EmailQueueApplication.java
@@ -0,0 +1,13 @@
+package com.email_queue.email_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email_queue/email_queue/EmailQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email_queue.email_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
