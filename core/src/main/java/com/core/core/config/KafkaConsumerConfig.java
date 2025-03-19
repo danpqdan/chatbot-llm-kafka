af5074a3a3a79dd30d893ee3ca73a3f0e4caa7cf
Daniel <danieltisantos@gmail.com>
2025-03-19 17:01:15 -0300

    [IMP] Refactor KafkaConsumerConfig to use kafkaConsumerProperties and add KafkaTemplate bean

--- core/src/main/java/com/core/core/config/KafkaConsumerConfig.java
+++ core/src/main/java/com/core/core/config/KafkaConsumerConfig.java
@@ -10,23 +10,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.sharedDTO.shared.dto.RequestDTO;
 
 @Configuration
 public class KafkaConsumerConfig {
 
     @Autowired
-    private KafkaProperties kafkaProperties;
+    private KafkaProperties kafkaConsumerProperties;
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaConsumerTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
-        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
+        Map<String, Object> props = kafkaConsumerProperties.buildConsumerProperties();
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        props.put(JsonDeserializer.TYPE_MAPPINGS, "requestDTO:com.core.core.dto.RequestDTO");
-        
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, RequestDTO.class);
+
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
@@ -36,4 +45,24 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
+
+    // @Bean
+    // public ConcurrentKafkaListenerContainerFactory<String, Object>
+    // kafkaListenerContainerFactory(
+    // KafkaTemplate<String, Object> kafkaTemplate) {
+
+    // ConcurrentKafkaListenerContainerFactory<String, Object> factory = new
+    // ConcurrentKafkaListenerContainerFactory<>();
+    // factory.setConsumerFactory(consumerFactory());
+
+    // // Configura um erro handler para enviar mensagens inválidas para um Dead
+    // Letter Topic
+    // factory.setCommonErrorHandler(new DefaultErrorHandler(
+    // new DeadLetterPublishingRecoverer(kafkaTemplate),
+    // new FixedBackOff(0L, 2)
+    // ));
+
+    // return factory;
+    // }
+
 }
