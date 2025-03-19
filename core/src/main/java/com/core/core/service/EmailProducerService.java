f481d486887cbdd0853223f52b72c4dccc6d4c3a
Daniel <danieltisantos@gmail.com>
2025-03-19 17:01:38 -0300

    [ADD] Implement EmailProducerService for sending messages to Kafka and handling responses

---
+++ core/src/main/java/com/core/core/service/EmailProducerService.java
@@ -0,0 +1,31 @@
+package com.core.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.sharedDTO.shared.dto.RequestDTO;
+
+@Service
+public class EmailProducerService {
+
+    @Value("${cliente.request.message.topic}")
+    private String clienteMessageRequestTopic;
+    
+    @Autowired
+    EmailConsumerService emailConsumerService;
+    @Autowired
+    @Qualifier("producerKafkaTemplate")
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendMessage(String username, String message) {
+        RequestDTO employeeResponse = new RequestDTO(
+                message,
+                "Employee Support:"+username);
+        emailConsumerService.responseMessage(username, employeeResponse);
+        kafkaTemplate.send(clienteMessageRequestTopic, employeeResponse);
+    }
+    
+}
